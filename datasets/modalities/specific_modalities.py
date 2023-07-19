bb9a1cfa05312da710fb217b6e7d7d8ce69fbede
bucuram <bucuram@ymail.com>
2023-07-19 18:09:26 +0200

    spliting into windoes not working

--- datasets/modalities/specific_modalities.py
+++ datasets/modalities/specific_modalities.py
@@ -9,8 +9,23 @@ class FaceLandmarks(Modality):
         self.modality_dir = 'face_landmarks'
 
     def post_process(self, data):
+
         data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
-        # TODO reshape / normalize etc
+
+        print('original shape')
+        print(data.shape)
+
+        data = np.asarray(np.split(data, self.args.n_temporal_windows, axis=0))
+
+        padding_max = int(self.args.seconds_per_window * self.args.max_video_fps)
+
+        if data.shape[0] < padding_max:
+            padding = np.zeros((padding_max - data.shape[0], data.shape[1], data.shape[2], data.shape[3], data.shape[4]))
+            data = np.concatenate((data, padding), axis=0)
+            
+        print('output_shape')
+        print(data.shape)
+
         return data
 
 
@@ -20,8 +35,9 @@ class HandLandmarks(Modality):
         self.modality_dir = 'hand_landmarks'
 
     def post_process(self, data):
+
         data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
-        # TODO reshape / normalize etc
+
         return data
 
 
@@ -31,8 +47,9 @@ class BodyLandmarks(Modality):
         self.modality_dir = 'body_pose_landmarks'
 
     def post_process(self, data):
+
         data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
-        # TODO reshape / normalize etc
+
         return data
 
 
@@ -42,7 +59,7 @@ class AudioEmbeddings(Modality):
         self.modality_dir = 'audio_pase_embeddings'
 
     def post_process(self, data):
-        # TODO reshape / normalize etc
+        # TODO add padding and stuff like that
         return data
 
 
@@ -52,5 +69,5 @@ class FaceEmbeddings(Modality):
         self.modality_dir = 'face_emonet_embeddings'
 
     def post_process(self, data):
-        # TODO reshape / normalize etc
+
         return data

77c82c6a973e949b5b70dcf1db2666b2f7bbd5be
bucuram <bucuram@ymail.com>
2023-07-27 15:28:46 +0200

    Revert "refactored stuff"
    This reverts commit ec4f6a0e9a0409074ad14f30a1eab729356d91dc.

--- models/modality_encoders.py
+++ models/modality_encoders.py
@@ -1,22 +1,22 @@
 import torch
 from einops import rearrange, repeat
-from .repeat import repeat as multi_sequential_repeat
 from einops.layers.torch import Reduce
 
 from .perceiver_blocks import TransformerLayer
 
 class NoOpEncoder(torch.nn.Module):
-    def __init__(self, args, modality_encoder_args):
+    def __init__(self, args, modality_encoder_args, modality_id):
         super(NoOpEncoder, self).__init__()
         self.args = args
         self.modality_encoder_args = modality_encoder_args
+        self.modality_id  = modality_id
 
         self.projection = torch.nn.Linear(
-            self.modality_encoder_args.input_dim,
+            self.modality_encoder_args.input_size,
             self.modality_encoder_args.model_args.latent_dim,
         )
 
-        max_fps = self.args.max_audio_fps if "audio" in self.modality_encoder_args.name else self.args.max_video_fps
+        max_fps = self.args.max_audio_fps if "audio" in self.modality_id else self.args.max_video_fps
         self.max_length = max_fps * self.args.seconds_per_window # it should be only one temporal window
 
         self.positional_embeddings = torch.nn.Embedding(
@@ -32,17 +32,18 @@ class NoOpEncoder(torch.nn.Module):
         return data
 
 class HandLandmarkEncoder(torch.nn.Module):
-    def __init__(self, args, modality_encoder_args):
+    def __init__(self, args, modality_encoder_args, modality_id):
         super(HandLandmarkEncoder, self).__init__()
         self.args = args
         self.modality_encoder_args = modality_encoder_args
+        self.modality_id = modality_id
 
         self.projection = torch.nn.Linear(
-            self.modality_encoder_args.input_dim,
+            self.modality_encoder_args.input_size,
             self.modality_encoder_args.model_args.latent_dim,
         )
 
-        self.hand_type_embeddings = torch.nn.Embedding(
+        self.hand_embeddings = torch.nn.Embedding(
             2, self.modality_encoder_args.model_args.latent_dim,
         )
 
@@ -51,8 +52,8 @@ class HandLandmarkEncoder(torch.nn.Module):
             self.modality_encoder_args.model_args.latent_dim,
         )
 
-        self.encoder = multi_sequential_repeat(
-            self.modality_encoder_args.model_args.num_layers,
+        self.encoder = repeat(
+            self.modality_encoder_args.num_layers,
             lambda _: TransformerLayer(
                 modality_encoder_args,
             ),
@@ -64,43 +65,37 @@ class HandLandmarkEncoder(torch.nn.Module):
         data = self.projection(data)
 
         # add positional embeddings
-        time_steps = torch.arange(self.args.max_video_fps * self.args.seconds_per_window).to(data.device)
-        time_steps = time_steps.repeat_interleave(2)
-        positions = self.positional_embeddings(time_steps)
-        positions = positions.reshape(self.args.max_video_fps * self.args.seconds_per_window, 2, self.modality_encoder_args.model_args.latent_dim)
-
-        data = data + positions
+        time_steps = torch.arange(self.max_length).to(data.device)
+        data = data + self.positional_embeddings(time_steps)
 
         # add token_types
-        hand_type_ids = torch.tensor([0, 1]).repeat(self.args.max_video_fps * self.args.seconds_per_window)
-        hand_type_ids = hand_type_ids.to(data.device)
+        token_type_ids = torch.tensor([0, 1]).repeat(self.args.max_video_fps * self.args.seconds_per_window)
+        token_type_ids = token_type_ids.to(data.device)
 
-        hand_embeddings = self.hand_type_embeddings(hand_type_ids)
-        hand_embeddings = hand_embeddings.reshape(self.args.max_video_fps * self.args.seconds_per_window, 2, self.modality_encoder_args.model_args.latent_dim)
+        token_embeddings = self.hand_embeddings(token_type_ids)
 
-        data = data + hand_embeddings
+        data = data + token_embeddings
 
         # process with encoder
-        data = data.view(data.shape[0], -1, self.modality_encoder_args.model_args.latent_dim)
-
-        mask = mask.repeat_interleave(2, dim = -1)
-        data, _ = self.encoder(data, mask)
+        data = data.view(data.shape[0], -1, self.modality_encoder_args.latent_dim)
+        data = self.encoder(data)
 
         # reshape again
-        data = data.view(data.shape[0], -1, 2, self.modality_encoder_args.model_args.latent_dim)
+        data = data.view(data.shape[0], -1, 2, self.modality_encoder_args.latent_dim)
         data = data.mean(dim = 2)
 
         return data
 
 
 class LandmarkEncoder(torch.nn.Module):
-    def __init__(self, args, modality_encoder_args):
+    def __init__(self, args, modality_encoder_args, modality_id):
         super(LandmarkEncoder, self).__init__()
         self.args = args
         self.modality_encoder_args = modality_encoder_args
+        self.modality_id = modality_id
 
         self.projection = torch.nn.Linear(
-            self.modality_encoder_args.input_dim,
+            self.modality_encoder_args.input_size,
             self.modality_encoder_args.model_args.latent_dim,
         )
 
@@ -109,8 +104,8 @@ class LandmarkEncoder(torch.nn.Module):
             self.modality_encoder_args.model_args.latent_dim,
         )
 
-        self.encoder = multi_sequential_repeat(
-            self.modality_encoder_args.model_args.num_layers,
+        self.encoder = repeat(
+            self.modality_encoder_args.num_layers,
             lambda _: TransformerLayer(
                 modality_encoder_args,
             ),
@@ -123,8 +118,7 @@ class LandmarkEncoder(torch.nn.Module):
         data = data.view(data.shape[0], data.shape[1], -1)
         data = self.projection(data)
 
-        time_steps = torch.arange(self.args.max_video_fps * self.args.seconds_per_window).to(data.device)
+        time_steps = torch.arange(self.max_length).to(data.device)
         data = data + self.positional_embeddings(time_steps)
-        data, _ = self.encoder(data, mask)
-
+        data = self.encoder(data, mask = mask)
         return data

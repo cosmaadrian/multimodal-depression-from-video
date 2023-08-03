adding landmark normalization via batchnorm1d + fixing dvlog_dataset.py bug

datasets/modalities/base_modality.py
@@ -149,7 +149,6 @@ class Modality(object):
         pad_data = np.pad(flatten_data, [(0,0), (0, dif_with_max), (0,0)], mode="constant", constant_values=0)
 
         # computing mask
-        # TODO check if mask is correct (1 = masked, 0 = not masked ???)
         no_modality_mask = np.pad(no_modality_mask, [(0,0), (0, dif_with_max)], mode="constant", constant_values=0)
 
         padding_mask = np.ones((W, T))
@@ -161,6 +160,4 @@ class Modality(object):
         original_shape = (W, -1) + data.shape[2:]
         out_data = np.reshape(pad_data, original_shape)
 
-        # TODO? N = the max length of the temporal window
-
-        return pad_data, mask.astype(bool)
+        return out_data, mask.astype(bool)

models/modality_encoders.py
@@ -38,6 +38,10 @@ class HandLandmarkEncoder(torch.nn.Module):
         self.args = args
         self.modality_encoder_args = modality_encoder_args
 
+        self.batch_norm  = torch.nn.BatchNorm1d(
+            self.modality_encoder_args.input_dim,
+        )
+
         self.projection = torch.nn.Linear(
             self.modality_encoder_args.input_dim,
             self.modality_encoder_args.model_args.latent_dim,
@@ -63,7 +67,21 @@ class HandLandmarkEncoder(torch.nn.Module):
         )
 
     def forward(self, data, mask):
-        data = data.view(data.shape[0], data.shape[1], 2, -1)
+        """Forward pass.
+        
+        Args:
+            data (torch.Tensor): input data (batch, time, hands, landmarks, coordinates)
+            mask (torch.Tensor): mask tensor (batch, time)
+
+        Returns:
+            torch.Tensor: modality encoder output tensor (batch, time, hands, latent_dim)
+        """
+        batch, time, _, _, _ = data.shape
+
+        data = data.view(batch, time * 2, -1).permute(0,2,1)
+        data = self.batch_norm(data).permute(0,2,1)
+
+        data = data.view(batch, time, 2, -1)
         data = self.projection(data)
 
         # add positional embeddings
@@ -101,6 +119,10 @@ class LandmarkEncoder(torch.nn.Module):
         self.args = args
         self.modality_encoder_args = modality_encoder_args
 
+        self.batch_norm  = torch.nn.BatchNorm1d(
+            self.modality_encoder_args.input_dim,
+        )
+
         self.projection = torch.nn.Linear(
             self.modality_encoder_args.input_dim,
             self.modality_encoder_args.model_args.latent_dim,
@@ -123,7 +145,18 @@ class LandmarkEncoder(torch.nn.Module):
         # TODO add batch normalization?
 
     def forward(self, data, mask):
-        data = data.view(data.shape[0], data.shape[1], -1)
+        """Forward pass.
+        
+        Args:
+            data (torch.Tensor): input data (batch, time, landmarks, coordinates)
+            mask (torch.Tensor): mask tensor (batch, time)
+
+        Returns:
+            torch.Tensor: modality encoder output tensor (batch, time, latent_dim)
+        """
+        data = data.view(data.shape[0], data.shape[1], -1).permute(0,2,1)
+        data = self.batch_norm(data).permute(0,2,1)
+
         data = self.projection(data)
 
         time_steps = torch.arange(self.max_data_length).to(data.device)
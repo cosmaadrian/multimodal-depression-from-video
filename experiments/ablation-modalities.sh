395383f3d282b9f32bf1fbe11190bf471bcc3d32
david-gimeno <dagigo1@dsic.upv.es>
2023-08-22 10:21:57 +0200

    Add other schedulers

--- experiments/ablation-modalities.sh
+++ experiments/ablation-modalities.sh
@@ -3,7 +3,7 @@
 set -e
 cd ..
 
-WANDB_MODE=run
+WANDB_MODE=dryrun
 BATCH_SIZE=8
 EPOCHS=500
 GROUP=baseline-modalities
@@ -14,6 +14,9 @@ AUDIO_VISUAL='audio_embeddings face_embeddings'
 ALL_MODALITIES='audio_embeddings face_embeddings face_landmarks body_landmarks hand_landmarks gaze_features blinking_features'
 
 # Audio-Visual [x]
+
+python main.py --run_id 1 --presence_threshold 0.5 --name av-lm-eyes-run-1 --seconds_per_window 5 --group $GROUP --mode $WANDB_MODE --epochs $EPOCHS --batch_size $BATCH_SIZE --config_file configs/baseline_config.yaml --env banamar-upv16 --use_modalities $ALL_MODALITIES
+exit -1
 
 # Audio-visual + landmarks + eyes
 python main.py --run_id 1 --presence_threshold 0.5 --name av-lm-eyes-run-1 --seconds_per_window 5 --group $GROUP --mode $WANDB_MODE --epochs $EPOCHS --batch_size $BATCH_SIZE --config_file configs/baseline_config.yaml --env banamar-upv16 --use_modalities $ALL_MODALITIES

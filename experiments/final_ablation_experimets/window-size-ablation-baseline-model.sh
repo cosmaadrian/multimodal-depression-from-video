49ad071279daed7cbcb86edbb8ab7cc4ed6ee0e1
bucuram <bucuram@ymail.com>
2023-08-24 17:10:05 +0200

    made some changes to ablation experiments scripts

--- experiments/final_ablation_experimets/window-size-ablation-baseline-model.sh
+++ experiments/final_ablation_experimets/window-size-ablation-baseline-model.sh
@@ -17,15 +17,15 @@ USE_MODALITIES="audio_embeddings face_embeddings"
 
 WANDB_MODE=dryrun
 ENV="banamar-upv16"
-GROUP=window-size-ablation-baseline-model
+GROUP=baseline-ws-ablation-final
 DEFAULT_ARGS="--group $GROUP --mode $WANDB_MODE --env $ENV --config_file configs/baseline_config.yaml --save_model 1 --trainer $TRAINER --scheduler $SCHEDULER --scheduler_args.max_lr $MAX_LR --n_temporal_windows $N_TEMPORAL_WINDOWS --presence_threshold $PRESENCE_THRESHOLD --epochs $EPOCHS --batch_size $BATCH_SIZE --use_modalities $USE_MODALITIES"
 
 
 for SECONDS_PER_WINDOW in $(seq $MIN_WINDOW_SIZE $MAX_WINDOW_SIZE)
 do
-    for RUN_ID in 1
-    do
-    WANDB_NAME="window-size-$SECONDS_PER_WINDOW-run-$RUN_ID"
-    python main.py --run_id $RUN_ID --name $WANDB_NAME $DEFAULT_ARGS --seconds_per_window $SECONDS_PER_WINDOW
+    for RUN_ID in 1 2 3
+        do
+        WANDB_NAME="window-size-$SECONDS_PER_WINDOW-run-$RUN_ID"
+        python main.py --run_id $RUN_ID --name $WANDB_NAME $DEFAULT_ARGS --seconds_per_window $SECONDS_PER_WINDOW
     done
 done

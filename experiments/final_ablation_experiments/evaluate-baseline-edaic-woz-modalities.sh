49febbb57394af8aa6913f20a8aac5ad5e0c535c
dagigo1 <dagigo1@dsic.upv.es>
2023-09-08 12:29:09 +0200

    e-daic exps details

--- experiments/final_ablation_experiments/evaluate-baseline-edaic-woz-modalities.sh
+++ experiments/final_ablation_experiments/evaluate-baseline-edaic-woz-modalities.sh
@@ -8,7 +8,7 @@ BATCH_SIZE=8
 EPOCHS=250
 SECONDS_PER_WINDOW=6
 
-GROUP=baseline-e-daic-woz-modalities
+GROUP=baseline-e-daic-woz-modalities+class-weights
 
 A_MODALITIES="edaic_audio_mfcc edaic_audio_egemaps"
 V_MODALITIES="edaic_video_cnn_resnet edaic_video_pose_gaze_aus"
@@ -16,28 +16,40 @@ AV_MODALITIES="edaic_audio_mfcc edaic_audio_egemaps edaic_video_cnn_resnet edaic
 
 
 # EVALUATING ON VALIDATION SET
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-4 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-5 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-4 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-5 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-4 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_val_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-5 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 
 # EVALUATING ON TEST SET
 python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-4 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audio-run-5 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
-# python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-4 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name video-run-5 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 
 python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-1 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-2 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-3 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-4 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
+python evaluate.py --eval_config  configs/eval_configs/eval_edaicwoz_test_config.yaml --output_dir $GROUP --checkpoint_kind best --name audiovisual-run-5 --n_temporal_windows 1 --seconds_per_window $SECONDS_PER_WINDOW --batch_size $BATCH_SIZE --group $GROUP --env banamar-upv16
 
